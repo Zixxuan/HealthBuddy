7999ed6cd99626cafa1780baac0f9f6d18ec2562
L31106 <155121j@mymail.nyp.edu.sg>
2017-09-06 14:00:09 +0800

    Add method to call HTTP GET

--- HealthBuddy/app/src/main/java/com/example/l31106/healthbuddy/MainActivity.java
+++ HealthBuddy/app/src/main/java/com/example/l31106/healthbuddy/MainActivity.java
@@ -8,8 +8,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView locationView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,20 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        locationView = (TextView) findViewById(R.id.locationText);
+
+
+        Button findButton = (Button)findViewById(R.id.locButton);
+        findButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                //Get locations from API Via HTTP GET
+
+                // Append result from json into locationView -> locationView.setText("");
             }
         });
     }
