67606eed71c720dd56f58e6c006d038e79936d3c
151292h <151292h@mymail.nyp.edu.sg>
2017-09-07 09:45:30 +0800

    HealthBuddy changed UI, add SideBar. V01.1

--- HealthBuddy/app/src/main/java/com/example/l31106/healthbuddy/MainActivity.java
+++ HealthBuddy/app/src/main/java/com/example/l31106/healthbuddy/MainActivity.java
@@ -3,19 +3,18 @@ package com.example.l31106.healthbuddy;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    TextView locationView;
-
-
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,28 +32,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        locationView = (TextView) findViewById(R.id.locationText);
 
 
-        Button findButton = (Button)findViewById(R.id.locButton);
-        findButton.setOnClickListener(new View.OnClickListener(){
 
-            @Override
-            public void onClick(View view){
-                //Get locations from API Via HTTP GET
-                //Testing for pull
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
-                //Pull test @
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
 
-                // Append result from json into locationView -> locationView.setText("");
-            }
-        });
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -71,5 +75,28 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_Learn) {
+            // Handle the camera action
+        } else if (id == R.id.nav_Direction) {
+
+        } else if (id == R.id.nav_Help) {
+
+        } else if (id == R.id.nav_settings) {
+
+        } else if (id == R.id.nav_aboutWayFinder) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
