5f9eae9cc90ddb10bd4c62fbf4d124960f96ae35
L31106 <155121j@mymail.nyp.edu.sg>
2017-09-07 17:33:48 +0800

    Add find button to learn location -Not done on-click, to use wifiManager

--- HealthBuddy/app/src/main/java/com/example/l31106/healthbuddy/learnClass.java
+++ HealthBuddy/app/src/main/java/com/example/l31106/healthbuddy/learnClass.java
@@ -1,22 +1,40 @@
 package com.example.l31106.healthbuddy;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by L31106 on 9/7/2017.
  */
 
 public class learnClass extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+       TextView locText;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +50,21 @@ public class learnClass extends AppCompatActivity implements NavigationView.OnNa
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        locText = (TextView) findViewById(R.id.locationText);
+        Button locButton = (Button)findViewById(R.id.learnLocButton);
+
+        locButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                // Use wifiManager to scan for AP
+
+            }
+        });
+
+
+
     }
 
 
@@ -64,7 +97,8 @@ public class learnClass extends AppCompatActivity implements NavigationView.OnNa
         int id = item.getItemId();
 
         if (id == R.id.nav_Learn) {
-
+            Intent learnIntent = new Intent(this, learnClass.class);
+            startActivity(learnIntent);
 
         } else if (id == R.id.nav_Direction) {
 
@@ -72,8 +106,35 @@ public class learnClass extends AppCompatActivity implements NavigationView.OnNa
 
         } else if (id == R.id.nav_Preferences) {
 
-            // do something
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(learnClass.this);
+            View mView = getLayoutInflater().inflate(R.layout.language_spinner, null);
+            mBuilder.setTitle("Select Your Preferred Language");
+            final Spinner mSpinner = (Spinner) mView.findViewById(R.id.spinnerLanguage);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(learnClass.this,
+                    android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.languageList));
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            mSpinner.setAdapter(adapter);
 
+            mBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    if (!mSpinner.getSelectedItem().toString().equalsIgnoreCase("Select Your Preferred Language")) {
+                        Toast.makeText(learnClass.this, mSpinner.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+                        dialogInterface.dismiss();
+                    }
+                }
+            });
+
+            mBuilder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+
+            mBuilder.setView(mView);
+            AlertDialog dialog = mBuilder.create();
+            dialog.show();
         } else if (id == R.id.nav_aboutWayFinder) {
 
         }
